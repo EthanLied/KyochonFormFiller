398ff13be89bd30a9ed47f4873210b144b6e2ad8
PC <ethanjy5428@gmail.com>
2026-08-20 23:56:44 +0800

    Scraping Bug Fix

--- src/app/scrape.tsx
+++ src/app/scrape.tsx
@@ -3,6 +3,7 @@
 import path from 'path';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import os from 'os';
 
 export interface scrapeResult {
   result: boolean
@@ -38,8 +39,8 @@ export async function formFiller(): Promise<scrapeResult> {
 
 
       // Goes to relative pages
-      await kyoChonPage.goto('https://loyalty.kyochon.com.my', { waitUntil: 'networkidle2' });
-      await tempMailPage.goto('https://temp-mail.org/en/', { waitUntil: 'networkidle2' })
+      await kyoChonPage.goto('https://loyalty.kyochon.com.my', { waitUntil: 'domcontentloaded' });
+      await tempMailPage.goto('https://temp-mail.org/en/', { waitUntil: 'domcontentloaded' })
 
 
       // Provide clipboard perms
@@ -56,7 +57,7 @@ export async function formFiller(): Promise<scrapeResult> {
 
       // Temp mail page
       await tempMailPage.bringToFront();
-      await (await tempMailPage.waitForSelector(`::-p-xpath(/html/body/div[3]/div/div/div[2]/div[1]/form/div[2]/button)`, { timeout: 3000 })).click();
+      await (await tempMailPage.waitForSelector(`::-p-xpath(/html/body/div[3]/div/div/div[2]/div[1]/form/div[1]/div/button[2])`, { timeout: 3000 })).click();
       email = await tempMailPage.evaluate(() => navigator.clipboard.readText());
       await kyoChonPage.bringToFront();
 
@@ -75,24 +76,24 @@ export async function formFiller(): Promise<scrapeResult> {
 
       password = randPass()
 
-      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[1]/div/input)`, { timeout: 300000 })).type(password);
-      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[2]/div/input)`, { timeout: 300000 })).type(password);
-      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[3]/div/input)`, { timeout: 300000 })).type(randFirst());
-      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[4]/div/input)`, { timeout: 300000 })).type(randLast());
+      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[3]/div/input)`, { timeout: 300000 })).type(password);
+      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[4]/div/input)`, { timeout: 300000 })).type(password);
+      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[5]/div/input)`, { timeout: 300000 })).type(randFirst());
+      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[6]/div/input)`, { timeout: 300000 })).type(randLast());
 
-      const genderSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[5]/div/div/select)`, { timeout: 300000 })
-      const daySelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[7]/div[1]/div/div/select)`, { timeout: 300000 })
-      const monthSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[7]/div[2]/div/div/select)`, { timeout: 300000 })
-      const yearSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[7]/div[3]/div/div/select)`, { timeout: 300000 })
-      const stateSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[8]/div/div/select)`, { timeout: 300000 })
+      const genderSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[7]/div/div/select)`, { timeout: 300000 })
+      const daySelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[8]/div[1]/div/div/select)`, { timeout: 300000 })
+      const monthSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[8]/div[2]/div/div/select)`, { timeout: 300000 })
+      const yearSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[8]/div[3]/div/div/select)`, { timeout: 300000 })
+      const stateSelect = await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[9]/div/div/select)`, { timeout: 300000 })
 
       await selectIndex(genderSelect, randSelect(1, 2))
-      await selectIndex(daySelect, randSelect(1, 31));
+      await selectIndex(daySelect, randSelect(1, 25));
       await selectIndex(monthSelect, randSelect(1, 12));
       await selectIndex(yearSelect, randSelect(10, 30));
       await selectIndex(stateSelect, randSelect(1, 12));
 
-      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[9]/input)`, { timeout: 300000 })).type("EWOYRJ");
+      await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/div[10]/input)`, { timeout: 300000 })).type("QD0785");
       await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/button)`, { timeout: 300000 })).click();
       await delay(2000);
       await (await kyoChonPage.waitForSelector(`::-p-xpath(/html/body/div[5]/div[1]/div/div[3]/div/div[2]/form/button)`, { timeout: 300000 })).click();
@@ -100,7 +101,7 @@ export async function formFiller(): Promise<scrapeResult> {
       await delay(20000);
 
       // Temp mail recieve code page
-      const emailLink = await tempMailPage.waitForSelector(`::-p-xpath(/html/body/main/div[1]/div/div[2]/div[2]/div/div[1]/div/div[4]/ul/li[3]/div[1]/a)`, { timeout: 300000 });
+      const emailLink = await tempMailPage.waitForSelector(`::-p-xpath(/html/body/main/div[1]/div/div[2]/div[2]/div/div[1]/div/div[4]/ul/li[2]/div[1]/a)`, { timeout: 300000 });
       const href = await emailLink.evaluate((el: Element) => el.getAttribute('href'));
 
       await tempMailPage.goto(href, { waitUntil: 'networkidle2' });
@@ -138,11 +139,15 @@ interface launchBrowserReturn { browser: any, kyoChonPage: any }
 async function launchBrowser({ proxy }: { proxy: boolean }): Promise<launchBrowserReturn> {
 
   puppeteer.use(StealthPlugin());
-
+  
   const browser = await puppeteer.launch({
     headless: false,
     ignoreHTTPSErrors: true,
+    executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', 
+    userDataDir: path.join(os.tmpdir(), 'fresh_edge_session'),
+    
     args: [
+      '--profile-directory=Profile 52',
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-quic',
